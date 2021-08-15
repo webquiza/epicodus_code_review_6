14307027453277089f8c8e0bb640526e21a59629
Carlos Urquiza <webquiza@gmail.com>
2021-08-14 21:28:24 -0700

    add static method and seperation of promise logic

---
+++ src/currency-service.js
@@ -0,0 +1,17 @@
+export default class CurrencyService {  
+  static getCurrency(base, target) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${base}/${target}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyService from './currency-service.js';
+
 
 
 
