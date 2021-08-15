0fdf40c3a210de82b239ef411daddeff0e300602
Carlos Urquiza <webquiza@gmail.com>
2021-08-14 22:30:23 -0700

    add document.ready function and clear fields method

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
-//import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyService from './currency-service.js';
+
+function clearFields() {
+  $('#base').val('');
+  $('#amount').val('');
+  $('#target').val('');
+}
+
+$(document).ready(function() {
+  $('#convert').click(function() {
+    const base = $('#base').val();
+    const target = $('#target').val();
+    const amount = parseFloat($('#amount').val());
+
+    clearFields();
+    let promise = CurrencyService.getCurrency(base, target);
+
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      const parseConvert = parseFloat(`${body.conversion_rate}`);
+      const resultConvert = (`${parseConvert}` * `${amount}`);
+
+      $('.showResult').text(`Your result is ${resultConvert} in ${target}`);
+      $('.showResult').val('');
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+      $('.showErrors').val('');
+    });
+  });
+});
 
 
 
