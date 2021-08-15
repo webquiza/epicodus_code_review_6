c1a752c6a6e0db54c6eb57dd2f09e95834c58ef2
Carlos Urquiza <webquiza@gmail.com>
2021-08-14 22:40:55 -0700

    refactor by removing document.ready function

--- src/index.js
+++ src/index.js
@@ -10,28 +10,27 @@ function clearFields() {
   $('#target').val('');
 }
 
-$(document).ready(function() {
-  $('#convert').click(function() {
-    const base = $('#base').val();
-    const target = $('#target').val();
-    const amount = parseFloat($('#amount').val());
+$('#convert').click(function() {
+  const base = $('#base').val();
+  const target = $('#target').val();
+  const amount = parseFloat($('#amount').val());
 
-    clearFields();
-    let promise = CurrencyService.getCurrency(base, target);
+  clearFields();
+  let promise = CurrencyService.getCurrency(base, target);
 
-    promise.then(function(response) {
-      const body = JSON.parse(response);
-      const parseConvert = parseFloat(`${body.conversion_rate}`);
-      const resultConvert = (`${parseConvert}` * `${amount}`);
+  promise.then(function(response) {
+    const body = JSON.parse(response);
+    const parseConvert = parseFloat(`${body.conversion_rate}`);
+    const resultConvert = (`${parseConvert}` * `${amount}`);
 
-      $('.showResult').text(`Your result is ${resultConvert} in ${target}`);
-      $('.showResult').val('');
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error}`);
-      $('.showErrors').val('');
-    });
+    $('.showResult').text(`Your result is ${resultConvert} in ${target}`);
+    $('.showResult').val('');
+  }, function(error) {
+    $('.showErrors').text(`There was an error processing your request: ${error}`);
+    $('.showErrors').val('');
   });
 });
+
 
 
 
